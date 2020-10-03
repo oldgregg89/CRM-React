72080ac4f0f534978334a8645feaa8b7c30edc92
Ian Gregg <iangregg188@gmail.com>
2020-10-03 16:09:56 -0700

    working on the details.

---
+++ src/components/ContactDetails.js
@@ -0,0 +1,9 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Button from "react-bootstrap/Button";
+
+function ContactDetail(props){
+  return (
+    <React.Fragment>
+      
+    </React.Fragment>
