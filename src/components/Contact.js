5b8cb39443b33f441a33861100368ce29ea788d4
Ian Gregg <iangregg188@gmail.com>
2020-10-03 13:09:35 -0700

    working on the contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,7 +5,7 @@ function Contact(props){
   return(
     <React.Fragment>
       <div onClick = {() => props.whenContactClicked(props.id)}>
-        <h3></h3>
+        <h3>{props.firstName}-{props.lastName}-{props.companyName}</h3>
         <hr/>
       </div>
     </React.Fragment>
@@ -13,5 +13,16 @@ function Contact(props){
 }
 
 Contact.PropTypes = {
-  
-}
+  firstName: PropTypes.string.isRequired,
+  lastName: PropTypes.string.isRequired,
+  phoneNumber: PropTypes.string.isRequired,
+  email: PropTypes.string.isRequired,
+  contract: PropTypes.number.isRequired,
+  companyName:PropTypes.string.isRequired,
+  notes: PropTypes.string,
+  contractClosed: PropTypes.bool,
+  id:PropTypes.string,
+  whenContactClicked:PropTypes.func
+};
+
+export default Contact;
