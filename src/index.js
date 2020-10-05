3d068bbc0faf95dc694d9107195f6df70cdbb0c7
Ian Gregg <iangregg188@gmail.com>
2020-10-05 16:16:13 -0700

    working on refactoring the code so the build will work within index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { ReactReduxFirebaseProvider } from 'react-redux-firebase';
+import { createFirestoreInstance } from 'redux-firestore';
+import firebase from "./firebase";
+import 'firebase/auth';
+
+const store = createStore(rootReducer);
+
+const rrfProps = {
+  firebase,
+  config: {
+      userProfile: "users",
+      useFirestoreForProfile: true
+    },
+  dispatch: store.dispatch,
+  createFirestoreInstance
+}
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store = {store}>
+    <ReactReduxFirebaseProvider {...rrfProps}>
+      <App />
+    </ReactReduxFirebaseProvider>
+  </Provider>,
   document.getElementById('root')
 );
 
