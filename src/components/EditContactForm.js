514641f40ba4bca39ba3a23d82c2ea275b988c0e
Ian Gregg <iangregg188@gmail.com>
2020-10-05 10:06:04 -0700

    working on the edit contact form.

---
+++ src/components/EditContactForm.js
@@ -0,0 +1,40 @@
+import React from "react";
+import ReusableForm from "./ReusableForm";
+import PropTypes from "prop-types";
+import { useFirestore } from 'react-redux-firebase';
+
+function EditContactForm(props){
+  const { contact } = props
+  const firestore = useFirestore();
+  function handleEditContactFormSubmission(event) {
+      event.preventDefault();
+      props.onEditContact();
+      console.log("here")
+      const propertiesToUpdate = {
+        firstName: event.target.firstName.value,
+        lastName: event.target.lastName.value,
+        phoneNumber: event.target.phoneNumber,
+        email: event.target.email,
+        contract: event.target.contract,
+        companyName: event.target.companyName,
+        notes: event.target.notes,
+        contractClosed: event.target.contractClosed,
+      }
+      return firestore.update({collection: "contact", doc: contact.id}, propertiesToUpdate)
+  }
+
+  return (
+    <React.Fragment>
+      <ReusableForm
+        formSubmissionHandler={handleEditContactFormSubmission}
+        contact={contact}
+        buttonText="Update Contact" />
+    </React.Fragment>
+  );
+}
+
+EditContactForm.propTypes = {
+  onEditContact:PropTypes.func
+};
+
+export default EditContactForm;
