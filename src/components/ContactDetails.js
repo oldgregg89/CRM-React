07b26f2594e09b5b0b29993477ac95dd988b58bf
Ian Gregg <iangregg188@gmail.com>
2020-10-05 09:31:54 -0700

    adding in the details of the contact.

--- src/components/ContactDetails.js
+++ src/components/ContactDetails.js
@@ -7,6 +7,17 @@ function ContactDetail(props){
     <React.Fragment>
       <h1><strong>Contact Details</strong></h1>
       <p><strong>First Name:</strong>{props.contact.firstName}</p>
+      <p><strong>Last Name:</strong>{props.contact.lastName}</p>
+      <p><strong>Phone Number:</strong>{props.contact.phoneNumber}</p>
+      <p><strong>Email:</strong>{props.contact.email}</p>
+      <p><strong>Current Contract Worth:</strong>{props.contact.contract}</p>
+      <p><strong>Company Name:</strong>{props.contact.companyName}</p>
+      <p><strong>Notes:</strong>{props.contact.notes}</p>
+      <p><strong>Contract Closed (T/F):</strong>{props.contact.contractClosed}</p>
+
+      <Button style={{margin: 10}} variant="success" onClick={props.onClickingEdit}>Edit Contact</Button>
+      <Button variant="danger" onClick={() => props.onClickingDelete(props.contact.id) }>Delete Contact</Button>
+      <hr/>
     </React.Fragment>
   );
 }
