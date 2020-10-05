a4d486aae0f7c587188887196e4859656d661335
Ian Gregg <iangregg188@gmail.com>
2020-10-05 09:10:42 -0700

    working on the details section of the application.

--- src/components/ContactDetails.js
+++ src/components/ContactDetails.js
@@ -5,5 +5,8 @@ import Button from "react-bootstrap/Button";
 function ContactDetail(props){
   return (
     <React.Fragment>
-      
+      <h1><strong>Contact Details</strong></h1>
+      <p><strong>First Name:</strong>{props.contact.firstName}</p>
     </React.Fragment>
+  );
+}
